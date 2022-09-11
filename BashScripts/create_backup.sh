352890be5ce8a7dbe990c7823546171f47857298
bokhanych <d.bakhanko@yandex.by>
2022-09-11 19:11:05 +0300

    add log

--- BashScripts/create_backup.sh
+++ BashScripts/create_backup.sh
@@ -3,6 +3,9 @@
 # date
 BACKUP_DATE=`date +%Y-%m-%d_%H:%M:%S`
 
+# log file
+LOG_FILE="/var/log/backups.log"
+
 # max count backup copies
 MAX_COPIES_COUNTER=5
 
@@ -18,7 +21,9 @@ if [ ! -d "$DIRECTORY_FOR_BACKUPS" ]; then
 fi
 
 # backup create
+echo "$DIRECTORY_FOR_BACKUPS/backup_$BACKUP_DATE.tar.bz2 DONE" >> $LOG_FILE
 tar -cjf $DIRECTORY_FOR_BACKUPS/backup_$BACKUP_DATE.tar.bz2 $DIRECTORY_TO_BACKUP 2>/dev/null
+echo "backup_$BACKUP_DATE.tar.bz2 DONE" >> $LOG_FILE
 
 # backup rotation
 COPIES_COUNTER=`ls $DIRECTORY_FOR_BACKUPS | grep backup_ | wc -l`
@@ -29,6 +34,7 @@ NEED_TO_DELETE_COUNTER=$(($(ls $DIRECTORY_FOR_BACKUPS | wc -l)-$MAX_COPIES_COUNT
 for (( i==1; i<$NEED_TO_DELETE_COUNTER; i++ ))
 do
 # sort and delete the older file
+echo "$(ls $DIRECTORY_FOR_BACKUPS | sort | head -n 1) REMOVED" >> $LOG_FILE
 rm $DIRECTORY_FOR_BACKUPS/$(ls $DIRECTORY_FOR_BACKUPS | sort | head -n 1)
 done
 fi
