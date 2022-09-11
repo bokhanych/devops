b2ce89894aafa5b9a76cb8e2a5b87e468ca13885
bokhanych <d.bakhanko@yandex.by>
2022-09-11 18:44:42 +0300

    done!

--- BashScripts/create_backup.sh
+++ BashScripts/create_backup.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-LOGFILE=/var/log/backups.log
-BACKUP_FOLDER=/home/BACKUP_FOLDER
-BACKUP_COUNT="5"
-FOLDER=/files
-FILE_COUNT=$(ls -l $BACKUP_FOLDER | grep -v ^d | wc -l)
+
+# date
+BACKUP_DATE=`date +%Y-%m-%d_%H:%M:%S`
+
+# max count backup copies
+MAX_COPIES_COUNTER=5
+
+# backup this directory
+DIRECTORY_TO_BACKUP="/files"
+
+# backup storage location
+DIRECTORY_FOR_BACKUPS="/home/backups"
+
 # check backup folder is exist
-if [ ! -d "$BACKUP_FOLDER" ]; then
-  mkdir -p $BACKUP_FOLDER;
-fi
-# create backup archive
-tar -czvf $BACKUP_FOLDER/backup.$(date +%Y%m%d).tar.gz $FOLDER
-
-
-
-if $FILE_COUNT > $BACKUP_COUNT; then
-echo "result: "
-ll $BACKUP_FOLDER
+if [ ! -d "$DIRECTORY_FOR_BACKUPS" ]; then
+  mkdir -p $DIRECTORY_FOR_BACKUPS;
 fi
 
-#rm -rf $(ls $FOLDER | head -1)
-#$ ls -1t | tail -n +11 | xargs rm -f
+# backup create
+tar -cjf $DIRECTORY_FOR_BACKUPS/backup_$BACKUP_DATE.tar.bz2 $DIRECTORY_TO_BACKUP 2>/dev/null
+
+# backup rotation
+COPIES_COUNTER=`ls $DIRECTORY_FOR_BACKUPS | grep backup_ | wc -l`
+if [ $COPIES_COUNTER -gt $MAX_COPIES_COUNTER ]
+then
+# how many files to delete
+NEED_TO_DELETE_COUNTER=$(($(ls $DIRECTORY_FOR_BACKUPS | wc -l)-$MAX_COPIES_COUNTER))
+for (( i==1; i<$NEED_TO_DELETE_COUNTER; i++ ))
+do
+# sort and delete the older file
+rm $DIRECTORY_FOR_BACKUPS/$(ls $DIRECTORY_FOR_BACKUPS | sort | head -n 1)
+done
+fi
