44a60b7c796c22e9a98f7e65961af0d697584db2
bokhanych <d.bakhanko@yandex.by>
2022-09-12 23:34:52 +0300

    in progress

--- BashScripts/file_genetator.sh
+++ BashScripts/file_genetator.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 GENERATION_FOLDER="/tmp"
-GENERATION_DEPTH="3"
+GENERATION_DEPTH="2"
+rm -r dir*
 
-# random <10
-$[$RANDOM % 9 +1 ]
+x="1"
+while [ $x -lt $GENERATION_DEPTH ]
+do
+((x++))
+DEPTH=3
+#       for ((i=1; i < $[$RANDOM % 9 +1 ]; i++))
+        for ((i=1; i < $DEPTH; i++))
+        do
+        mkdir dir$i
+                for ((f=1; f < $[$RANDOM % 9 +1 ]; f++))
+                do
+                        touch dir$i/file$i$[$RANDOM % 9 +1 ]
+                done
+        done
+done
